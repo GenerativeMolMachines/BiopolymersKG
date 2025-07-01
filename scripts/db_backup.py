06261fd8689828c56c786851a5ecaec6d693df62
NIkolay Shirokiy <nikolai@niuitmo.ru>
2025-07-01 14:39:35 +0300

    add: db backuper class

---
+++ scripts/db_backup.py
@@ -0,0 +1,52 @@
+import os
+import sys
+import logging
+
+import dotenv
+from yadisk import YaDisk
+
+dotenv.load_dotenv()
+
+LOCAL_BACKUPS_LOCATION = os.environ["LOCAL_BACKUPS_LOCATION"]
+REMOTE_BACKUPS_LOCATION = "/backups"
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+
+class Backuper:
+    def __init__(self):
+        self.client = self.connect_yadisk()
+
+    @staticmethod
+    def connect_yadisk() -> YaDisk:
+        yadisk_client = YaDisk(token=os.environ["YADISK_TOKEN"])
+        yadisk_client.check_token()
+
+        return yadisk_client
+
+    def check_existing_backups(self) -> list[str | None]:
+        if not self.client.exists(REMOTE_BACKUPS_LOCATION):
+            self.client.mkdir(REMOTE_BACKUPS_LOCATION)
+            return []
+        files = [i.name for i in self.client.listdir(REMOTE_BACKUPS_LOCATION)]
+        return files
+
+    def upload_backups(self):
+        if not os.path.exists(LOCAL_BACKUPS_LOCATION):
+            raise Exception(f"Local backups directory {LOCAL_BACKUPS_LOCATION} does not exist.")
+
+        existing_backups = self.check_existing_backups()
+        not_loaded_backups = [i for i in os.listdir(LOCAL_BACKUPS_LOCATION) if i not in existing_backups]
+
+        for f in not_loaded_backups:
+            self.client.upload(
+                os.path.join(LOCAL_BACKUPS_LOCATION, f),
+                os.path.join(REMOTE_BACKUPS_LOCATION, f),
+                overwrite=True,
+                timeout=100
+            )
+
+if __name__ == "__main__":
+    backuper = Backuper()
+    backuper.upload_backups()
